5a3193be3730daa3096ef44250fca20c57c29a41
Ben <david@davids-iMac-2.local>
2014-10-20 14:08:12 +1100

    first commit

---
+++ 12WBTWorkout/SoundSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SoundSettingViewController.h
+//  12WBTWorkout
+//
+//  Created by david on 6/12/13.
+//  Copyright (c) 2013 david. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SoundSettingViewController : UIViewController
+
+@end
---
+++ 12WBTWorkout/SoundSettingViewController.m
@@ -0,0 +1,38 @@
+//
+//  SoundSettingViewController.m
+//  12WBTWorkout
+//
+//  Created by david on 6/12/13.
+//  Copyright (c) 2013 david. All rights reserved.
+//
+
+#import "SoundSettingViewController.h"
+
+@interface SoundSettingViewController ()
+
+@end
+
+@implementation SoundSettingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
